993e946982b20eb611a0e154e5d92386963f1b4d
Robert Bost <rbost@redhat.com>
2016-02-18 10:51:07 -0500

    removed port arg

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ```
 # gcc lookup.c -o lookup $(apr-1-config --cflags --cppflags --includes --link-ld) -ggdb
 # ./lookup 
-Usage: ./lookup host port
+Usage: ./lookup host
 
-# ./lookup google.com 80
-google.com:80 => 173.194.123.96
+# ./lookup google.com
+google.com => 173.194.123.96
 ```
--- lookup.c
+++ lookup.c
@@ -5,23 +5,21 @@
 
 /**
  * # gcc lookup.c -o lookup $(apr-1-config --cflags --cppflags --includes --link-ld)
- * # ./lookup google.com 80
+ * # ./lookup google.com
  * 
- * @author bostrt
+ * @author rbost@redhat.com
  */
 int main(int argc, char *argv[]) {
 
-  if (argc < 2) {
-    printf("Usage: %s host port\n", argv[0]);
+  if (argc < 1) {
+    printf("Usage: %s host\n", argv[0]);
     return APR_EGENERAL;
   }
   
   apr_status_t rv;
   apr_pool_t *pool;
-  apr_pool_t *pool_data;
   apr_sockaddr_t *addr;
   char *host = argv[1];
-  apr_port_t port = atoi(argv[2]);
 
   rv = apr_initialize();
   if (rv != APR_SUCCESS) {
@@ -29,20 +27,18 @@ int main(int argc, char *argv[]) {
     return APR_EGENERAL;
   }
   
-  apr_pool_create(&pool_data, NULL);
   apr_pool_create(&pool, NULL);
 
-  rv = apr_sockaddr_info_get(&addr, host, APR_UNSPEC, port, APR_IPV4_ADDR_OK, pool);
+  rv = apr_sockaddr_info_get(&addr, host, APR_UNSPEC, 0, APR_IPV4_ADDR_OK, pool);
 
   if (rv != APR_SUCCESS) {
     printf("APR apr_sockaddr_info_get err\n");
     return APR_EGENERAL;
   }
 
-  printf("%s:%d => %s\n", addr->hostname, addr->port, inet_ntoa(addr->sa.sin.sin_addr));
+  printf("%s => %s\n", addr->hostname, inet_ntoa(addr->sa.sin.sin_addr));
 
   apr_pool_destroy(pool);
-  apr_pool_destroy(pool_data);
   apr_terminate();
 
   return APR_SUCCESS;
